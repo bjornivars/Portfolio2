98fde17e698e8c8ece6d19a68399512f946a4f97
Bjørn-Ivar Skuggen <bjorn-ivar.skuggen@hotmail.com>
2020-04-05 19:46:28 +0200

    going to test with react reveal

--- client/src/components/work.js
+++ client/src/components/work.js
@@ -1,14 +1,57 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const WorkPage = () => {
     return (
         <div>
             <div className='project'>
                 <div className='project-container'>
-                    <div className='project-container-img col-8'>
+                    <div className='project-container-img col-7 col-mobile-12'>
+                        <img className='col-10 col-mobile-12' src={require('./../assets/ProjectImages/infotech.png')} alt='Project Infotech' />
                     </div>
-                    <div className='project-container-text col-4'>
-                        <h2>ProjectName</h2>
+                    <div className='project-container-text col-5 col-mobile-12'>
+                        <h2>INFOTECH <br /> Website Redesign</h2>
+                        <p className='project-container-p col-10 col-mobile-12'>Infotech AS is a company located
+                        in Bergen, Norway. During the first
+                        year of my studies, we were asked to
+                        do a redesign of their website. </p>
+                        <Link className='btn' to=''>Read More</Link>
+                    </div>
+                </div>
+                <div className='col-2 m-auto'>
+                    <img className='col-12' src={require('./../assets/Icons/workSplitter.png')} alt='divider' />
+                </div>
+                <div className='project-container row-reverse '>
+                    <div className='project-container-text col-5 col-mobile-12'>
+                        <h2>LETHA <br /> E-commerce example</h2>
+                        <p className='project-container-p col-10 col-mobile-12'>
+                            This was an e-commerce website
+                            example from my first year of
+                            school.
+                         </p>
+                        <Link className='btn' to=''>Read More</Link>
+                    </div>
+                    <div className='project-container-img col-7 col-mobile-12 order'>
+                        <img className='col-10 col-mobile-12' src={require('./../assets/ProjectImages/letha.png')} alt='Project Infotech' />
+                    </div>
+                </div>
+                <div className='col-2 m-auto'>
+                    <img className='col-12' src={require('./../assets/Icons/workSplitter.png')} alt='divider' />
+                </div>
+                <div className='project-container'>
+                    <div className='project-container-img col-7 col-mobile-12'>
+                        <img className='col-10 col-mobile-12' src={require('./../assets/ProjectImages/GameOfDragons.png')} alt='Project Infotech' />
+                    </div>
+                    <div className='project-container-text col-5 col-mobile-12'>
+                        <h2>Game Of  Dragons
+                        <br /> Board Game</h2>
+                        <p className='project-container-p col-10 col-mobile-12'>
+                            Game Of Dragons was a project where
+                            I made a Game of Thrones themed
+                            Board Game. The game is built with
+                            HTML, CSS and JavaScript.
+                         </p>
+                        <Link className='btn' to=''>Read More</Link>
                     </div>
                 </div>
             </div>
--- client/src/pages/Work.js
+++ client/src/pages/Work.js
@@ -10,8 +10,9 @@ export default function Work() {
         <div className='container-paddingSide'>
             <h1 className='headline'>Here are some of my projects <br />
             during these two years at school.</h1>
+        </div>
             <WorkPage />
-            </div>
+            
         </div>
     )
 }
