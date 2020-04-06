78f19ca3355035d31f272ae5dc37b0475d29f5c4
Bjørn-Ivar Skuggen <bjorn-ivar.skuggen@hotmail.com>
2020-04-06 04:04:09 +0200

    working on it

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,8 @@ import React from 'react';
 import HomePage from './../components/home';
 
 export default function Home() {
-
+    console.log('I am aware of warning componentWillReceiveProps. There is no current fix for this issue yet.');
+    console.log('Forum link for warning: https://www.bountysource.com/issues/78548951-refactor-componentwillreceiveprops-in-revealbase')
     return (
         <div className='container'>
             <HomePage />
