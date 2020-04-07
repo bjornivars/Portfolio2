e2779484504ec9472c0f5cf4dfbee37224e77a3a
Bjørn-Ivar Skuggen <bjorn-ivar.skuggen@hotmail.com>
2020-04-07 16:48:27 +0200

    testing out react-hook-form after this commit

--- client/src/components/contact.js
+++ client/src/components/contact.js
@@ -6,43 +6,42 @@ const ContactPage = () => {
     return (
         <div className='contact'>
 
-            <form className='contact-form col-10'>
+            <form className='contact-form col-10 col-mobile-12'>
                 <Bounce left>
-                    <div className='col-12'>
+                    <div className='col-12 l-mobile-12'>
                         <label for="fname" className='contact-form-label'>First Name</label>
                         <input className='contact-form-input' type="text" name="firstname" />
                     </div>
                 </Bounce>
                 <Bounce left delay={100}>
-                <div className='col-12'>
-
+                <div className='col-12 l-mobile-12'>
                     <label for="lname" className='contact-form-label'>Last Name</label>
                     <input className='contact-form-input' type="text" name="lastname" />
                 </div>
                 </Bounce>
                 <Bounce left delay={200}>
-                <div className='col-12'>
+                <div className='col-12 l-mobile-12'>
 
                     <label for="email" className='contact-form-label'>Email</label>
                     <input className='contact-form-input' type="email" name="email" />
                 </div>
                 </Bounce>
                 <Bounce left delay={300}>
-                <div className='col-12'>
+                <div className='col-12 l-mobile-12'>
 
                     <label for="number" className='contact-form-label'>Phone number</label>
                     <input className='contact-form-input' type="text" name="phone" />
                 </div>
                 </Bounce>
                 <Bounce left delay={400}>
-                <div className='col-12'>
+                <div className='col-12 l-mobile-12'>
 
                     <label for="subject" className='contact-form-label'>Message</label>
                     <textarea className='contact-form-input' name="message"></textarea>
                 </div>
                 </Bounce>
                 <Bounce left delay={500}>
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Submit" className='btn'/>
                 </Bounce>
             </form>
         </div>
